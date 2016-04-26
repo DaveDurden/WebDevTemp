6278fc2544e99b4feaa35547d84edec136d0830f
Christian Hoffmann <hoffmann@wernerbohr.de>
2016-04-26 15:33:09 +0200

    add initial gulp setup
    – full gulp workflow (incl. sass, autoprefix, minifying, linting, image optimization, browsersync server, notifications, watch tasks, ...)
    – npm package.json
    – basic index.html template
    – sass 7-1 architecture (incl. normalize)
    – jQuery & modernizr

---
+++ gulpfile.js
@@ -0,0 +1,160 @@
+// include gulp
+var gulp            = require('gulp');
+
+// include SASS & CSS plugins
+var sass            = require('gulp-sass'),
+    sassGlob        = require('gulp-sass-glob'),
+    cssnano         = require('gulp-cssnano'),
+    pixrem          = require('gulp-pixrem'),
+    autoprefixer    = require('gulp-autoprefixer');
+
+// include JS plugins
+var jshint          = require('gulp-jshint'),
+    uglify          = require('gulp-uglify'),
+    sourcemaps      = require('gulp-sourcemaps');
+
+// include file operation plugins
+var concat          = require('gulp-concat'),
+    rename          = require('gulp-rename'),
+    cache           = require('gulp-cache'),
+    del             = require('del');
+
+// include other plugins
+var notify          = require('gulp-notify'),
+    browserSync     = require('browser-sync').create(),
+    runSequence     = require('run-sequence'),
+    imagemin        = require('gulp-imagemin');
+
+
+// copy markup & text files to dist folder
+gulp.task('html', function(){
+    del(['dist/*.+(html|php|txt)']);
+    return gulp.src('src/*.+(html|php|txt)')
+        .pipe(cache(gulp.dest('dist')))
+        ;
+});
+
+
+// concatenate, compile, prefix & minify styles
+gulp.task('styles', function() {
+    return gulp.src('src/sass/main.scss')
+        .pipe(sassGlob())
+        .pipe(sass({
+            style: 'expanded',
+            sourceComments: true,
+            indentWidth: 4
+        }))
+        .on('error', notify.onError({
+            title: 'Error compiling SASS',
+            message: '<%= error.message %>',
+            sound: true,
+            wait: true
+        }))
+        .pipe(autoprefixer('> 1% in DE'))
+        .pipe(pixrem({
+            rootValue: '16px',
+            atrules: true,
+            replace: false
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'))
+        ;
+});
+
+
+// optimize static images
+gulp.task('images', function() {
+    del(['dist/img']);
+    return gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
+        .pipe(cache(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/img'))
+        ;
+});
+
+
+// copy font files
+gulp.task('fonts', function() {
+    del(['dist/fonts']);
+    return gulp.src('src/fonts/**/*')
+        .pipe(cache(gulp.dest('dist/fonts')))
+        ;
+});
+
+
+// lint scripts
+gulp.task('lint', function() {
+    return gulp.src(['src/js/**/*.js', '!src/js/vendor/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        ;
+});
+
+
+// copy vendor scripts
+gulp.task('vendor-scripts', function() {
+    return gulp.src('src/js/vendor/**/*.js')
+        .pipe(cache(gulp.dest('dist/js/vendor')))
+        ;
+});
+
+
+// concatenate & minify javascripts
+gulp.task('scripts', ['lint', 'vendor-scripts'], function() {
+    return gulp.src(['src/js/**/*.js', '!src/js/vendor/**/*.js'])
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        ;
+});
+
+
+// run tasks when a file changes
+gulp.task('watch', function() {
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/sass/**/*.scss', ['styles']);
+    gulp.watch(['src/js/**/*.js', '!src/js/vendor/**/*.js'], ['scripts']);
+    gulp.watch('src/img/**/*', ['images']);
+    gulp.watch('src/fonts/**/*', ['fonts']);
+});
+
+
+// start browsersync server
+gulp.task('serve', ['watch'], function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist/',
+        },
+        browser: 'google chrome',
+        files: ['dist/**/*'],
+        notify: false,
+        reloadDelay: 500
+    });
+    // for manual reload: browserSync.reload();
+});
+
+
+// default task (called with 'gulp')
+gulp.task('default', ['serve']);
+
+
+// clean dist folder
+gulp.task('clean', function() {
+    cache.clearAll();
+    return del(['dist']);
+});
+
+
+// rebuild task for full site regeneration
+gulp.task('rebuild', function() {
+    runSequence('clean', ['html', 'styles', 'images', 'fonts', 'scripts']);
+});
